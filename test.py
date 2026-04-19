3acd4fbc3004f9eb49e759e033c8a3880bcdc8e7
Golu Singh <golusingh94712@gmail.com>
2026-04-19 19:07:29 +0530

    Update test.py
    Imporvements like
    
    Custom file upload and prediction has been done

--- test.py
+++ test.py
@@ -1,21 +1,35 @@
+from PIL import Image
 import matplotlib.pyplot as plt
+from torchvision import transforms
+from google.colab import files
+import torch
 
-# Get one sample image
-image, label = train_data[0]
+# Upload image
+uploaded = files.upload()
 
-# Move to device
+# Get file
+img_path = list(uploaded.keys())[0]
+image = Image.open(img_path).convert("RGB")
+
+# Transform
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+img_tensor = transform(image).unsqueeze(0).to(device)
+
+# Predict
 model.eval()
 with torch.no_grad():
-    outputs = model(pixel_values=image.unsqueeze(0).to(device))
+    outputs = model(pixel_values=img_tensor)
     pred = outputs.logits.argmax(dim=1).item()
 
-# Class names
-classes = train_data.classes
+# Class labels
+classes = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
 
-# Show image
-plt.imshow(image.permute(1, 2, 0))
+# Show result
+plt.imshow(image)
 plt.title(f"Predicted: {classes[pred]}")
 plt.axis('off')
 plt.show()
-
-print("Actual:", classes[label])
